afc38ff2ba4cc28dc83bf3929a7e502643888099
jndewey <jndewey@gmail.com>
2013-03-17 16:29:53 -0400

    custom routes finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Postcloser::Application.routes.draw do
   devise_for :users
 
+
+  devise_scope :user do
+    get 'register', to: 'devise/registrations#new', as: :register 
+     get 'login', to: 'devise/sessions#new', as: :login 
+       get 'logout', to: 'devise/sessions#destroy', as: :logout 
+  end
+
   resources :closings
 
-root to: 'closings#index'
+  get 'feed', to: 'closings#index', as: :feed
+
+  root to: 'closings#index'
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- db/migrate/20130226023405_add_user_id_to_closing.rb
+++
@@ -1,8 +0,0 @@
-class AddUserIdToClosing.rb < ActiveRecord::Migration
-  def change
-    add_column :closings, :user_id, :integer
-    add_index :closings, :user_id
-    remove_column :closings, :name
-  end
-end
-	
