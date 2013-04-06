f19cada16fba4ad6e005b5c452710e4032f2cb34
Joe <jndewey@gmail.com>
2013-04-06 15:37:45 -0400

    one more time

--- db/migrate/20130406190436_remove_team_member_from_closings.rb
+++
@@ -1,9 +0,0 @@
-class RemoveTeamMemberFromClosings < ActiveRecord::Migration
-  def up
-    remove_column :closings, :teammember
-  end
-
-  def down
-    add_column :closings, :teammember, :string
-  end
-end
