37eda5996f7d0c3f1403d79467e16bd1ba83311d
Joe <jndewey@gmail.com>
2013-04-22 21:38:13 -0400

    mig fix

--- db/migrate/20130421125040_remove_tasks_from_closing_items.rb
+++
@@ -1,9 +0,0 @@
-class RemoveTasksFromClosingItems < ActiveRecord::Migration
-  def up
-    remove_column :closing_items, :tasks
-  end
-
-  def down
-    add_column :closing_items, :tasks, :string
-  end
-end
